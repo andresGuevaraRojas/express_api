cbc21a9431c66824f881ce0d595b231a1121c7e0
Andres Guevara <=>
2022-04-24 16:36:36 -0500

    Se agrega el endpoint /v1/explorers:id con metodo PUT

--- app.js
+++ app.js
@@ -38,3 +38,13 @@ app.post('/v1/explorers',(req,res)=>{
     
     res.status(201).json({message:'Created'})
 })
+
+
+app.put('/v1/explorers/:id',(req,res)=>{
+    console.log(`Api explorers PUT requests ${new Date()}`)
+    console.log(`Update explorer with ${req.params.id}`)
+
+    const requestBody = req.body
+    
+    res.status(200).json({message:'Updated'})
+})
